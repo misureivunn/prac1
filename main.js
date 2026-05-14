71a46ff633f0ee5cee4261ebe0926acc32a25068
misureivunn <zhimbeevaryuna@gmail.com>
2026-05-14 12:05:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const DataAccessLayer = require('./dataAccess');
 const BusinessLogicLayer = require('./businessLogic');
 const PresentationLayer = require('./present');
 
-// Делаем main асинхронной
 async function main() {
     const dataLayer = new DataAccessLayer();
     const businessLayer = new BusinessLogicLayer(dataLayer);
     const presentationLayer = new PresentationLayer(businessLayer);
     
-    // Запускаем и ждем
     await presentationLayer.run();
 }
 
